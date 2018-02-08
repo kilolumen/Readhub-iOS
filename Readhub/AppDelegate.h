d28dd29af008e099c20ca8b30936818de680915d
lidongjie <lidongjie-rj@ofo.com>
2018-02-08 14:53:45 +0800

    Initial project

---
+++ Readhub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Readhub
+//
+//  Created by lidongjie on 2018/2/8.
+//  Copyright © 2018年 kilolumen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Readhub/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Readhub
+//
+//  Created by lidongjie on 2018/2/8.
+//  Copyright © 2018年 kilolumen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
